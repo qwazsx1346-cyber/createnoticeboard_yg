4d7453428275dcc6ef1744bf1393aa7b3b7b3b07
younggeun <qwazsx1346@gmail.com>
2026-01-29 13:06:37 +0900

    git 첫 commit

---
+++ src/main/java/com/green/createnoticeboard_yg/CreatenoticeboardYgApplication.java
@@ -0,0 +1,13 @@
+package com.green.createnoticeboard_yg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatenoticeboardYgApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CreatenoticeboardYgApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/green/createnoticeboard_yg/CreatenoticeboardYgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.createnoticeboard_yg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatenoticeboardYgApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
